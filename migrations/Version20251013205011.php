2d634ff264c5943a7ea90a64f5227578daaf24b6
Peter Lachky <petiar@petiar.sk>
2025-10-13 23:54:24 +0200

    migrations

---
+++ migrations/Version20251013205011.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251013205011 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE api_key (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, key_value VARCHAR(64) NOT NULL, created_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_C912ED9DE7927C74 (email), UNIQUE INDEX UNIQ_C912ED9DFF4925FB (key_value), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE api_key');
+    }
+}
