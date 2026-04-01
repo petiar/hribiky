77dc6688fe94cc0d1df0f20dc7f8106264b27eba
Peter Lachky <petiar@petiar.sk>
2026-04-02 00:33:17 +0200

    leaderboard API

---
+++ src/Controller/LeaderboardApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api/leaderboard', name: 'api_leaderboard_')]
+class LeaderboardApiController extends AbstractController
+{
+    #[Route('', name: 'index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): JsonResponse
+    {
+        $contributors = $userRepository->findTopContributors(100);
+
+        $data = array_map(fn($user) => [
+            'name' => $user->getName(),
+            'count' => $user->getMushroomCount(),
+        ], $contributors);
+
+        return $this->json($data);
+    }
+}
