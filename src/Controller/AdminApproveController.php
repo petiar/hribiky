f06c3db315d2246bf7d96ff892605ee52223df72
Peter Lachky <petiar@petiar.sk>
2026-04-04 18:13:18 +0200

    mushroom approval enhancement

--- src/Controller/AdminApproveController.php
+++ src/Controller/AdminApproveController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class AdminApproveController extends AbstractController
 {
-    #[Route('/admin/approve/{token}', name: 'admin_mushroom_approve')]
+    #[Route('/mushroom/approve/{token}', name: 'admin_mushroom_approve')]
     public function approve(string $token, MushroomApprovalService $approvalService): Response
     {
         $mushroom = $approvalService->approveByToken($token);
@@ -17,7 +17,8 @@ class AdminApproveController extends AbstractController
         if (!$mushroom) {
             throw $this->createNotFoundException('Odkaz je neplatný alebo už bol použitý.');
         }
+        $this->addFlash('success', 'Hríbik bol schválený, si super!');
 
-        return $this->redirectToRoute('app_login');
+        return $this->redirectToRoute('mushroom_detail', ['id' => $mushroom->getId()]);
     }
 }
